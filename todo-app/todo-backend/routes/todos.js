719a2816d98d59109dbb06fbd0f18a9415791705
Markus Becks <markus.becks@gmail.com>
2023-08-19 18:24:25 +0300

    add get and put routes

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require('express')
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
@@ -29,21 +29,37 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.get('/:id', async (req, res) => {
+  const todo = req.todo
+  try {
+    res.status(200).send(todo)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ message: 'Internal server error' })
+  }
+})
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.put('/:id', async (req, res) => {
+  const updatedTodo = req.todo
+  const { text, done } = req.body
+  try {
+    await updatedTodo.save({
+      text,
+      done,
+    })
+    res.status(200).send(updatedTodo)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ message: 'Internal server error' })
+  }
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
